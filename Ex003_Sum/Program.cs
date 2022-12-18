f104b408f29bb68c4838f24a760088fc2d59ea03
ElenaSelina <120081986+ElenaSelina@users.noreply.github.com>
2022-12-18 19:57:17 +0700

    Added Ex003

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
+﻿int numberA = new Random();
 int numberB = 5;
+int numberA = new Random().Next(1, 10); // 1 2 3 4 5 ... 9
+int numberB = new Random().Next(1, 10); // 1 2 3 4 5 ... 9
 int result = numberA + numberB;
 Console.WriteLine(result);
