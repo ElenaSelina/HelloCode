bc9c735fb02e3c92256067e799030ba1cb47197e
ElenaSelina <120081986+ElenaSelina@users.noreply.github.com>
2023-01-30 16:50:40 +0700

    Ex034

---
+++ Ex052/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int[,] array = { {1, 4, 7, 2},
+                    {5, 9, 2, 3},
+                    {8, 4, 2, 4} };
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for(int i = 0; i < columns; i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < rows; j++)
+        {
+            sum += array[j,i];
+        }
+        double average = (double)sum / rows;
+        Console.Write(average + " ");
+    }
+
+  }
+}
