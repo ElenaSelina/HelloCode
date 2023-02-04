d00ea29f6788775282cee2e182bb8ad8f38b8d0f
ElenaSelina <120081986+ElenaSelina@users.noreply.github.com>
2023-02-04 22:26:37 +0700

    Task054 added

--- Ex054/Program.cs
+++ Ex054/Program.cs
@@ -8,30 +8,27 @@
 9 5 3 2
 8 4 4 2 */
 
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[][] arr = new int[3][];
+
+        arr[0] = new int[] { 1, 4, 7, 2 };
+        arr[1] = new int[] { 5, 9, 2, 3 };
+        arr[2] = new int[] { 8, 4, 2, 4 };
+
+        int rows = arr.GetLength(0);
+        int columns = arr[0].GetLength(0);
+
+        for (int i = 0; i < rows; i++)
         {
-            int[,] arr = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
-            int rows = arr.GetLength(0);
-            int columns = arr.GetLength(1);
+            Array.Sort(arr[i], new Comparison<int>((i1, i2) => i2.CompareTo(i1)));
+        }
 
-            for (int i = 0; i < rows; i++)
-            {
-                Array.Sort(arr, i * columns, columns, new Comparison<int>((a, b) => b - a));
-            }
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < columns; j++)
-                {
-                    Console.Write(arr[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
+        for (int i = 0; i < rows; i++)
+        {
+            Console.WriteLine("number list: " + string.Join(",", arr[i]));
         }
     }
 }
-
-
