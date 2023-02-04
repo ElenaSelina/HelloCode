4f7d8fa3e1f91228693f6d0486f1cebce79888dc
ElenaSelina <120081986+ElenaSelina@users.noreply.github.com>
2023-02-04 22:27:46 +0700

    Task062 added

--- Ex062/Program.cs
+++ Ex062/Program.cs
@@ -5,55 +5,56 @@
 11 16 15 06
 10 09 08 07 */
 
-using System;
-
-namespace ConsoleApp
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[,] arr = new int[4, 4];
+        int value = 1;
+        int minRow = 0;
+        int maxRow = arr.GetLength(0) - 1;
+        int minCol = 0;
+        int maxCol = arr.GetLength(1) - 1;
+
+        while (value <= arr.Length)
         {
-            int[,] arr = new int[4, 4];
-            int value = 1;
-            int minRow = 0, maxRow = arr.GetLength(0) - 1;
-            int minCol = 0, maxCol = arr.GetLength(1) - 1;
-
-            while (value <= arr.Length)
+            for (int i = minRow; i <= maxRow; i++)
             {
-                for (int i = minRow; i <= maxRow; i++)
-                {
-                    arr[i, minCol] = value++;
-                }
-                minCol++;
+                arr[i, minCol] = value++;
+            }
+            minCol++;
 
-                for (int i = minCol; i <= maxCol; i++)
-                {
-                    arr[maxRow, i] = value++;
-                }
-                maxRow--;
+            for (int i = minCol; i <= maxCol; i++)
+            {
+                arr[maxRow, i] = value++;
+            }
+            maxRow--;
 
-                for (int i = maxRow; i >= minRow; i--)
-                {
-                    arr[i, maxCol] = value++;
-                }
-                maxCol--;
+            for (int i = maxRow; i >= minRow; i--)
+            {
+                arr[i, maxCol] = value++;
+            }
+            maxCol--;
 
-                for (int i = maxCol; i >= minCol; i--)
-                {
-                    arr[minRow, i] = value++;
-                }
-                minRow++;
+            for (int i = maxCol; i >= minCol; i--)
+            {
+                arr[minRow, i] = value++;
+            }
+            minRow++;
+        }
+
+        Program.PrintMatrix(arr);
+    }
+
+    public static void PrintMatrix(int[,] matrix) {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[j,i] + "\t");
             }
 
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    Console.Write(arr[i, j].ToString().PadLeft(2, '0') + " ");
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine();
         }
     }
 }
-
