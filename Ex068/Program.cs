4c636b79ab0ca325b516da3a006a5531d7c4c7d2
ElenaSelina <120081986+ElenaSelina@users.noreply.github.com>
2023-02-04 22:28:26 +0700

    Task068 added

--- Ex068/Program.cs
+++ Ex068/Program.cs
@@ -1,35 +1,33 @@
-﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана
+с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29 */
 
-namespace Task68
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Enter m: ");
-            int m = int.Parse(Console.ReadLine());
-            Console.Write("Enter n: ");
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ackermann: " + Ackermann(m, n));
+        Console.Write("Enter m: ");
+        string? strM = Console.ReadLine();
+        Console.Write("Enter n: ");
+        string? strN = Console.ReadLine();
+
+        if (strM == null || strN == null) {
+            throw new Exception("illegal argument");
         }
 
-        static int Ackermann(int m, int n)
-        {
-            if (m == 0)
-            {
-                return n + 1;
-            }
-            if (m > 0 && n == 0)
-            {
-                return Ackermann(m - 1, 1);
-            }
-            if (m > 0 && n > 0)
-            {
-                return Ackermann(m - 1, Ackermann(m, n - 1));
-            }
-            return 0;
-        }
+        int m = int.Parse(strM);
+        int n = int.Parse(strN);
+
+        Console.WriteLine("Ackermann: " + Ackermann(m, n));
+    }
+
+    static int Ackermann(int m, int n)
+    {
+        return (
+            (m == 0) ? n + 1 :
+            (n == 0) ? Ackermann(m - 1, 1) :
+            Ackermann(m - 1, Ackermann(m, n - 1))
+        );
     }
 }
